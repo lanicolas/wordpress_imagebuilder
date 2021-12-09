aee52e08df849ebe00fc21bd99b8e8bee40781da
lanicolas <lauranicolas87@outlook.com>
2021-12-09 10:11:33 +0100

    Gitops

--- scripts/wordpress_install.sh
+++ scripts/wordpress_install.sh
@@ -15,23 +15,23 @@ systemctl start apache2
 systemctl enable apache2  
 
 apt-get install -y php php-mysql libapache2-mod-php
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $db_root_pwd"  
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $db_root_pwd"  
+echo "mysql-server-5.6 mysql-server/root_password password $db_root_pwd" | sudo debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password $db_root_pwd" | sudo debconf-set-selections
 apt-get install -y mysql-client mysql-server unzip
 
 rm /var/www/html/index.*  
 curl -O https://wordpress.org/latest.zip
-unzip latest.zip
+unzip -o latest.zip
 mv wordpress/* /var/www/html/  
 chown -R www-data:www-data /var/www/html/  
 chmod -R 755 /var/www/html/  
 
-mysql -uroot -p$db_root_pwd <<EOF 
+mysql -uroot -p$db_root_pwd <<BASH_QUERY 
 CREATE DATABASE $wordpress_db;  
 GRANT ALL PRIVILEGES ON $wordpress_db.* TO 'root'@'localhost' IDENTIFIED BY '$db_root_pwd';  
 FLUSH PRIVILEGES;  
-EXIT  
-EOF  
+quit  
+BASH_QUERY  
 
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php  
 sed -i "s/database_name_here/$wordpress_db/g" /var/www/html/wp-config.php  
